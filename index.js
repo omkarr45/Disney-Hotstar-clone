0ae6c57debcdfdc3d3e55d59afbed096d9df62fe
omkarr45 <omkarpote945@gmail.com>
2024-05-30 12:39:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,110 @@
+let movies = [
+  {
+    name: "CHLESEA VS NEWCASTLE UNITED",
+    des:
+      "Watch the video 'Newcastle 1-1 Chelsea' & also get latest Football updates & highlights online only on Disney + Hotstar.",
+    image: "images/football.jpg"
+  },
+{
+  name: "Avatar: The Way of Water",
+    des:
+    "Jake Sully and Ney'tiri have formed a family and are doing everything to stay together. However, they must leave their home and explore the regions of Pandora. When an ancient threat resurfaces, Jake must fight a difficult war against the humans."  ,
+      image: "images/avatar.jpg"
+},
+{
+  name: "ROHIT CAPTAINCY'S TEST IN CARIBBEAN",
+    des:
+    "3 challenges for Rohit Sharma as Team India kick start World Cup 2023 preparations",
+        image: "images/rohit1.jpg"
+},
+{
+  name: "IND VS WI",
+    des:
+  "1st ODI: All You Need to Know - Hotstar.",
+    image: "images/ind vs wi.jpg"
+},
+
+  ];
+
+const carousel = document.querySelector(".carousel");
+let sliders = [];
+
+let slideIndex = 0; // track the current slide
+
+const createSlide = () => {
+  if (slideIndex >= movies.length) {
+    slideIndex = 0;
+  }
+
+  // Create DOM Elements
+  let slide = document.createElement("div");
+  var imgElement = document.createElement("img");
+  let content = document.createElement("div");
+  let h1 = document.createElement("h1");
+  let p = document.createElement("p");
+
+  // attaching all elements
+  imgElement.appendChild(document.createTextNode(""));
+  h1.appendChild(document.createTextNode(movies[slideIndex].name));
+  p.appendChild(document.createTextNode(movies[slideIndex].des));
+  content.appendChild(h1);
+  content.appendChild(p);
+  slide.appendChild(content);
+  slide.appendChild(imgElement);
+  carousel.appendChild(slide);
+
+  // setting up image
+  imgElement.src = movies[slideIndex].image;
+  slideIndex++;
+
+  // setting elements classname
+  slide.className = "slider";
+  content.className = "slide-content";
+  h1.className = "movie-title";
+  p.className = "movie-des";
+
+  sliders.push(slide);
+
+  if (sliders.length) {
+    sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${30 * (sliders.length - 2)
+      }px)`;
+  }
+};
+
+for (let i = 0; i < 3; i++) {
+  createSlide();
+}
+setInterval(() => {
+  createSlide();
+}, 3000);
+
+//video cards
+
+const videocards = [...document.querySelectorAll(".video-card")];
+videocards.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    let video = item.children[1];
+    video.play();
+  });
+  item.addEventListener("mouseleave", () => {
+    let video = item.children[1];
+    video.pause();
+  });
+});
+
+//card sliders
+
+let cardcontainers = [...document.querySelectorAll(".card-container")];
+let preBtns = [...document.querySelectorAll(".pre-btn")];
+let nxtBtns = [...document.querySelectorAll(".nxt-btn")];
+
+cardcontainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+  nxtBtns[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth - 200;
+  });
+  preBtns[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth + 200;
+  });
+});
